8f1f7a62ee4156e6248d3634c67317bea7887e8f
marcelo-feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-04 02:10:46 -0300

    Created instantiation operation

--- src/main/java/com/feliciano/demo/DemoApplication.java
+++ src/main/java/com/feliciano/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.feliciano.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.feliciano.demo.repositories.CategoriaRepository;
+import com.feliciano.demo.resources.domain.Categoria;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
