abfedc65f09d16e17b2b50377135442fbfb81801
marcelo-feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-04 02:34:42 -0300

    Created class produto and association many-to-many

--- src/main/java/com/feliciano/demo/DemoApplication.java
+++ src/main/java/com/feliciano/demo/DemoApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.feliciano.demo.repositories.CategoriaRepository;
+import com.feliciano.demo.repositories.ProdutoRepository;
 import com.feliciano.demo.resources.domain.Categoria;
+import com.feliciano.demo.resources.domain.Produto;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
@@ -25,7 +29,19 @@ public class DemoApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 
+		Produto p1 = new Produto(null, "Computador", 2999.0);
+		Produto p2 = new Produto(null, "Impressora", 599.0);
+		Produto p3 = new Produto(null, "Teclado", 245.0);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat1);
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 	}
 
--- src/main/java/com/feliciano/demo/resources/domain/Produto.java
+++ src/main/java/com/feliciano/demo/resources/domain/Produto.java
@@ -9,10 +9,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable {
+public class Produto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -20,17 +22,22 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"), 
+		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<>();
 
-	public Categoria(Integer id, String nome) {
-		this.id = id;
-		this.nome = nome;
+	public Produto() {
 	}
 
-	public Categoria() {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
 	}
 
 	/**
@@ -62,22 +69,36 @@ public class Categoria implements Serializable {
 	}
 
 	/**
-	 * @return the produtos
+	 * @return the preco
 	 */
-	public List<Produto> getProdutos() {
-		return produtos;
+	public Double getPreco() {
+		return preco;
 	}
 
 	/**
-	 * @param produtos the produtos to set
+	 * @param preco the preco to set
 	 */
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	/**
+	 * @return the categorias
+	 */
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	/**
+	 * @param categorias the categorias to set
+	 */
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, nome);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -88,8 +109,8 @@ public class Categoria implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
