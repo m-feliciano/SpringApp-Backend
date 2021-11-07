522cee4daecc82f9470cb5b7badbe258efbedce1
marcelo-feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-07 05:19:57 -0300

    Inserting a new POST "categoria"

--- src/main/java/com/feliciano/demo/resources/CategoriaResource.java
+++ src/main/java/com/feliciano/demo/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.feliciano.demo.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.feliciano.demo.resources.domain.Categoria;
 import com.feliciano.demo.services.CategoriaService;
@@ -21,6 +25,15 @@ public class CategoriaResource {
 	private ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) { //@RequestBody converts JSON to object body
+		obj = service.insert(obj);
+		ServletUriComponentsBuilder.fromCurrentRequest();
+		URI uri = ServletUriComponentsBuilder.fromPath("/{id}")
+				.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
