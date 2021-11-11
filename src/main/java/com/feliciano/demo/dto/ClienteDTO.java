8de08772df7220455f2f7874c3449075c7da4367
Marcelo Feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-11 00:01:45 -0300

    customizing validation error

---
+++ src/main/java/com/feliciano/demo/dto/ClienteDTO.java
@@ -0,0 +1,73 @@
+package com.feliciano.demo.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.feliciano.demo.resources.domain.Cliente;
+import javax.validation.constraints.Email; 
+
+public class ClienteDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+
+	@NotEmpty(message = "Preenchiento obrigatório")
+	@Length(min = 5, max = 14, 
+		message = "A categoria '${validatedValue}' deve ter entre {min} e {max} characteres")
+	private String nome;
+
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		id = obj.getId();
+		nome = obj.getNome();
+	}
+
+	public ClienteDTO(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the serialversionuid
+	 */
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
---
+++ src/main/java/com/feliciano/demo/resources/exceptions/FieldMessage.java
@@ -0,0 +1,42 @@
+package com.feliciano.demo.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String fildName;
+	private String message;
+
+	public FieldMessage() {
+		super();
+	}
+
+	public FieldMessage(String fildName, String message) {
+		super();
+		this.fildName = fildName;
+		this.message = message;
+	}
+
+	public String getFildName() {
+		return fildName;
+	}
+
+	public void setFildName(String fildName) {
+		this.fildName = fildName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+}
