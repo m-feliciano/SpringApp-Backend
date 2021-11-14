0cd220ea5c3c2f930e7f1dd73b93e8092361aaa1
Marcelo Feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-14 02:15:13 -0300

    find products by nome e categoria

--- src/main/java/com/feliciano/demo/DemoApplication.java
+++ src/main/java/com/feliciano/demo/DemoApplication.java
@@ -70,16 +70,35 @@ public class DemoApplication implements CommandLineRunner {
 		Produto prod1 = new Produto(null, "Computador", 2999.0);
 		Produto prod2 = new Produto(null, "Impressora", 599.0);
 		Produto prod3 = new Produto(null, "Teclado", 245.0);
+		Produto prod4 = new Produto(null, "Mesa", 569.0);
+		Produto prod5 = new Produto(null, "Monitor", 999.5);
+		Produto prod6 = new Produto(null, "Perfume", 105.8);
+		Produto prod7 = new Produto(null, "Sabonete", 9.25);
+		Produto prod8 = new Produto(null, "Toalha", 19.30);
+		Produto prod9 = new Produto(null, "Tinta", 45.9);
 
 		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
-		cat2.getProdutos().addAll(Arrays.asList(prod2));
-
-		prod1.getCategorias().add(cat1);
+		cat2.getProdutos().addAll(Arrays.asList(prod2, prod4, prod5));
+		cat3.getProdutos().addAll(Arrays.asList(prod8));
+		cat4.getProdutos().addAll(Arrays.asList(prod9, prod4));
+		cat5.getProdutos().addAll(Arrays.asList(prod3, prod5));
+		cat6.getProdutos().addAll(Arrays.asList(prod6));
+		cat7.getProdutos().addAll(Arrays.asList(prod7));
+		
+		prod1.getCategorias().addAll(Arrays.asList(cat1,cat5));
 		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-		prod3.getCategorias().add(cat1);
-
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2,cat3, cat4, cat5, cat6, cat7));
-		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+		prod3.getCategorias().addAll(Arrays.asList(cat1,cat5));
+		prod4.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		prod5.getCategorias().addAll(Arrays.asList(cat1, cat2, cat5));
+		prod6.getCategorias().addAll(Arrays.asList(cat6,cat7));
+		prod7.getCategorias().addAll(Arrays.asList(cat6,cat7));
+		prod8.getCategorias().addAll(Arrays.asList(cat3));
+		prod9.getCategorias().addAll(Arrays.asList(cat4));
+				
+		categoriaRepository.saveAll(Arrays.asList(
+				cat1, cat2,cat3, cat4, cat5, cat6, cat7));
+		produtoRepository.saveAll(Arrays.asList(
+				prod1, prod2, prod3, prod4, prod5, prod6, prod7, prod8, prod9));
 
 		Estado est1 = new Estado(null, "São Paulo");
 		Estado est2 = new Estado(null, "Minas Gerais");
