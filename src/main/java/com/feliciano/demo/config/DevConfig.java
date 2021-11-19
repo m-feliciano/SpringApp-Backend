49634f46e41be42afab38146d5ff6c305c44d32a
Marcelo Feliciano <marcelo.feliciano2@fatec.sp.gov.br>
2021-11-19 16:04:49 -0300

    Dev profile created

--- src/main/java/com/feliciano/demo/config/DevConfig.java
+++ src/main/java/com/feliciano/demo/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.feliciano.demo.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,16 +11,23 @@ import org.springframework.context.annotation.Profile;
 import com.feliciano.demo.services.DBService;
 
 @Configuration
-@Profile("test")
-public class TestConfig {
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
 
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String profile;
+
 	@Bean
 	public boolean instanciateDatabase() throws ParseException {
 
+		if (!"create".equals(profile)) {
+			return false;
+		}
 		dbService.instanciateTestDatabase();
+
 		return true;
 	}
 
