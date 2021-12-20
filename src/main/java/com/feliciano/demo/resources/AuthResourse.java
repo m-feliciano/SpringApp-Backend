934b7cdc5f0ae8d941ff1dd990c4337e753a61d8
Marcelo Feliciano <60995044+m-feliciano@users.noreply.github.com>
2021-12-20 02:03:56 -0300

    refresh token

---
+++ src/main/java/com/feliciano/demo/resources/AuthResourse.java
@@ -0,0 +1,28 @@
+package com.feliciano.demo.resources;
+
+import com.feliciano.demo.security.JWTUtil;
+import com.feliciano.demo.security.SpringSecurityUser;
+import com.feliciano.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResourse {
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        SpringSecurityUser user = UserService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        return ResponseEntity.noContent().build();
+    }
+}
